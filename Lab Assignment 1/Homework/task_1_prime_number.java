6ee55585da9984de17bae1124e02ae5d7721c657
Algo-Pilot <fahimasfaq01@gmail.com>
2025-07-07 03:45:45 +0600

    update the commit message for the new file added

---
+++ Lab Assignment 1/Homework/task_1_prime_number.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class task_1_prime_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        boolean isPrime;
+        int count = 0;
+        
+        if(end<start){
+            int a = start;
+            start = end;
+            end = a;
+        }
+        for (int i = start; i <= end; i++) {
+            if(i<2){
+                continue;
+            }
+            isPrime = true;
+            for(int j = 2; j <= i; j++) {
+                if(i % j == 0 && j != i) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime ) {
+                count++;
+            }
+        }
+        System.err.println("There are "+ count +" prime numbers between "+start+" and "+end+".");
+    }
+}
+
+
+    
