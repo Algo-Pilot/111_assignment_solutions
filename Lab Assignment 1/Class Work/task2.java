d7023090c7112308b1cbbb92949aabbe8b7c436a
Algo-Pilot <fahimasfaq01@gmail.com>
2025-07-20 06:25:11 +0600

    new file

---
+++ Lab Assignment 1/Class Work/task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class task2{
+    public static void main(String [] args){
+        Scanner sc =  new Scanner (System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String concat = s1 + " "+s2;
+        int sum = 0;
+        for(int i = 0; i < concat.length(); i++){
+            int a = concat.charAt(i);
+            if((a >= 65 && a<=90) || (a>=97 && a<=122)){
+                sum += a;
+            }
+        }
+        System.out.println(concat);
+        System.out.println(sum);
+    }
+}
