8e3c2f85027be3d1096a4ab120410895b938d5a0
Algo-Pilot <fahimasfaq01@gmail.com>
2025-07-20 06:09:50 +0600

    new

---
+++ Lab Assignment 1/Class Work/task1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class task1 {
+    public static void main(String [] args){
+        Scanner sc =  new Scanner (System.in);
+        int sum = 0;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        int count = 0;
+        for(int i = 0; i < 10; i++){
+            int a = sc.nextInt();
+            if(a % 2 ==1 && a > 0){
+                sum += a;
+                count++;
+                if(a< min){
+                    min = a;
+                }
+                if(a> max){
+                    max =a;
+                }
+            }
+            
+        }
+        System.out.println("Sum = " + sum);
+        System.out.println("Minimum = " + min);
+        System.out.println("Maximum = " + max);
+        double avg = sum / (double)count;
+        System.out.println("Average = " + avg);
+        sc.close();
+    }
+}
